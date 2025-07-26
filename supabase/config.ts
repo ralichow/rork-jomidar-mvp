6c30d4d1cdfe6ecedb2fb8afdd5eaca36e23c2f9
Tazwar Mohammed Shoumik <tazwarmohammed1998@gmail.com>
2025-07-26 23:41:18 +0600

    Add supabase confiurations in config.ts file

--- supabase/config.ts
+++ supabase/config.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://ysallicdlgqajzjrjicr.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYWxsaWNkbGdxYWp6anJqaWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MzMxMjMsImV4cCI6MjA2OTEwOTEyM30.rv9ruHxVNTLLJaHxkcx6YOvG8ilO70XkhFtFoT-5z50'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
